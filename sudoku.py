3fb9f5fe21d95c5efbe1e59b67c630830bf73724
McMac31 <miguel317.angelps@gmail.com>
2025-11-20 12:33:25 +0100

    Clase sudoku creada, Pasar partida a JSON

--- sudoku.py
+++ sudoku.py
@@ -1,106 +1,114 @@
 #Primero crear la matriz 9x9
 from random import random, randint
 import time
+import json
 
-GameOver=False
-matriz=[[" "," "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "," "]
-        
-]
+class Sudoku:
+    def __init__(self):
+        self.GameOver=False
+        self.matriz=[[" "," "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "," "]
+        ]
 
-def Presentacion():
-    print(""" 
-██████╗ ██╗███████╗███╗   ██╗██╗   ██╗███████╗███╗   ██╗██╗██████╗  ██████╗ 
-██╔══██╗██║██╔════╝████╗  ██║██║   ██║██╔════╝████╗  ██║██║██╔══██╗██╔═══██╗
-██████╔╝██║█████╗  ██╔██╗ ██║██║   ██║█████╗  ██╔██╗ ██║██║██║  ██║██║   ██║
-██╔══██╗██║██╔══╝  ██║╚██╗██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║██║  ██║██║   ██║
-██████╔╝██║███████╗██║ ╚████║ ╚████╔╝ ███████╗██║ ╚████║██║██████╔╝╚██████╔╝
-╚═════╝ ╚═╝╚══════╝╚═╝  ╚═══╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝╚═╝╚═════╝  ╚═════╝ """)
-    time.sleep(1)
-    print("""   
- █████╗ ██╗     
-██╔══██╗██║     
-███████║██║     
-██╔══██║██║     
-██║  ██║███████╗
-╚═╝  ╚═╝╚══════╝""")
-    time.sleep(1)
+    def Presentacion(self):
+        print(""" 
+    ██████╗ ██╗███████╗███╗   ██╗██╗   ██╗███████╗███╗   ██╗██╗██████╗  ██████╗ 
+    ██╔══██╗██║██╔════╝████╗  ██║██║   ██║██╔════╝████╗  ██║██║██╔══██╗██╔═══██╗
+    ██████╔╝██║█████╗  ██╔██╗ ██║██║   ██║█████╗  ██╔██╗ ██║██║██║  ██║██║   ██║
+    ██╔══██╗██║██╔══╝  ██║╚██╗██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║██║  ██║██║   ██║
+    ██████╔╝██║███████╗██║ ╚████║ ╚████╔╝ ███████╗██║ ╚████║██║██████╔╝╚██████╔╝
+    ╚═════╝ ╚═╝╚══════╝╚═╝  ╚═══╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝╚═╝╚═════╝  ╚═════╝ """)
+        time.sleep(1)
+        print("""   
+    █████╗ ██╗     
+    ██╔══██╗██║     
+    ███████║██║     
+    ██╔══██║██║     
+    ██║  ██║███████╗
+    ╚═╝  ╚═╝╚══════╝""")
+        time.sleep(1)
 
-    print("""  
-███████╗██╗   ██╗██████╗  ██████╗ ██╗  ██╗██╗   ██╗██╗██╗
-██╔════╝██║   ██║██╔══██╗██╔═══██╗██║ ██╔╝██║   ██║██║██║
-███████╗██║   ██║██║  ██║██║   ██║█████╔╝ ██║   ██║██║██║
-╚════██║██║   ██║██║  ██║██║   ██║██╔═██╗ ██║   ██║╚═╝╚═╝
-███████║╚██████╔╝██████╔╝╚██████╔╝██║  ██╗╚██████╔╝██╗██╗
-╚══════╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚═╝
-                                                                       """)
-    time.sleep(2)
-
-
-
-def MostrarTablero():
-    print("     0   1   2     3   4   5     6   7   8")   
-    print("    ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
-  
+        print("""  
+    ███████╗██╗   ██╗██████╗  ██████╗ ██╗  ██╗██╗   ██╗██╗██╗
+    ██╔════╝██║   ██║██╔══██╗██╔═══██╗██║ ██╔╝██║   ██║██║██║
+    ███████╗██║   ██║██║  ██║██║   ██║█████╔╝ ██║   ██║██║██║
+    ╚════██║██║   ██║██║  ██║██║   ██║██╔═██╗ ██║   ██║╚═╝╚═╝
+    ███████║╚██████╔╝██████╔╝╚██████╔╝██║  ██╗╚██████╔╝██╗██╗
+    ╚══════╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚═╝
+                                                                        """)
+        time.sleep(2)
+        self.nombre= input("Cual es tu nombre? ")
+        self.apellido= input("Cual es tu apellido? ")
+    def MostrarTablero(self):
+        print("     0   1   2     3   4   5     6   7   8")   
+        print("    ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
     
-    for i in range(len(matriz)):
-        tab=f""" {i} ▐ {matriz[i][0]} ▐ {matriz[i][1]} ▐ {matriz[i][2]} |▓| {matriz[i][3]} ▐ {matriz[i][4]} ▐ {matriz[i][5]} |▓| {matriz[i][6]} ▐ {matriz[i][7]} ▐ {matriz[i][8]} ▐ """
-        print(tab)
-        if i==2 or i==5:
-            print("    ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
-    print("    ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+        
+        for i in range(len(self.matriz)):
+            tab=f""" {i} ▐ {self.matriz[i][0]} ▐ {self.matriz[i][1]} ▐ {self.matriz[i][2]} |▓| {self.matriz[i][3]} ▐ {self.matriz[i][4]} ▐ {self.matriz[i][5]} |▓| {self.matriz[i][6]} ▐ {self.matriz[i][7]} ▐ {self.matriz[i][8]} ▐ """
+            print(tab)
+            if i==2 or i==5:
+                print("    ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+        print("    ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
 
 
-#Meter numeros aleatorios que no se repitan en indices aleatorios
-def numerosIniciales():
-      for i in range(20):#Se pone a 300 para probar mas rapidamente
-        rellenoAleatorio=randint(0,9)
-        filAleatoria=randint(0,8)
-        colAleatoria=randint(0,8)
-        matriz[filAleatoria][colAleatoria]=rellenoAleatorio
+    #Meter numeros aleatorios que no se repitan en indices aleatorios
+    def numerosIniciales(self):
+        for i in range(300):#Se pone a 300 para probar mas rapidamente
+            rellenoAleatorio=randint(1,9)
+            filAleatoria=randint(0,8)
+            colAleatoria=randint(0,8)
+            self.matriz[filAleatoria][colAleatoria]=rellenoAleatorio
+    
+    def crearDiccionario(self):
+        datos_estudiante={"nombre":self.nombre,
+                          "apellido":self.apellido,
+                          "api_url":"",
+                          "sudoku":self.matriz
+                          }
+        return datos_estudiante
 
 
-#hacer que el usuario meta numeros en posicion indicada
-def rellenarPosiciones():
-    global GameOver
-    while(GameOver==False):
-        jugadaUsuario= int(input("Ingrese el numero para rellenar la posicion " ))
-        fila=int(input("En que fila ingresara el numero? " ))
-        columna=int(input("En que columna ingresara el numero? " ))
-        matriz[fila][columna]=jugadaUsuario
-        if all(posiciones != " " for fila in matriz for posiciones in fila):
-            GameOver=True
-        MostrarTablero()
+
+    #hacer que el usuario meta numeros en posicion indicada
+    def rellenarPosiciones(self):
+        while(self.GameOver==False):
+            jugadaUsuario= int(input("Ingrese el numero para rellenar la posicion " ))
+            fila=int(input("En que fila ingresara el numero? " ))
+            columna=int(input("En que columna ingresara el numero? " ))
+            self.matriz[fila][columna]=jugadaUsuario
+            if all(posiciones != " " for fila in self.matriz for posiciones in fila):
+                self.GameOver=True
+            self.MostrarTablero()
+            
+    def partida(self):
+        self.Presentacion()
+        self.numerosIniciales()
+        self.MostrarTablero()
+        self.rellenarPosiciones()
+
+        if self.GameOver== True:
+            print("""      
+        ███████╗███████╗     █████╗  ██████╗ █████╗ ██████╗  ██████╗     ███████╗██╗              ██╗██╗   ██╗███████╗ ██████╗  ██████╗ 
+        ██╔════╝██╔════╝    ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔═══██╗    ██╔════╝██║              ██║██║   ██║██╔════╝██╔════╝ ██╔═══██╗
+        ███████╗█████╗      ███████║██║     ███████║██████╔╝██║   ██║    █████╗  ██║              ██║██║   ██║█████╗  ██║  ███╗██║   ██║
+        ╚════██║██╔══╝      ██╔══██║██║     ██╔══██║██╔══██╗██║   ██║    ██╔══╝  ██║         ██   ██║██║   ██║██╔══╝  ██║   ██║██║   ██║
+        ███████║███████╗    ██║  ██║╚██████╗██║  ██║██████╔╝╚██████╔╝    ███████╗███████╗    ╚█████╔╝╚██████╔╝███████╗╚██████╔╝╚██████╔╝
+        ╚══════╝╚══════╝    ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═════╝  ╚═════╝     ╚══════╝╚══════╝     ╚════╝  ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝ """)
+            datos=self.crearDiccionario()
+            with open("Datos.json","w+") as archivo:
+                json.dump(datos,archivo,indent=4)
+
         
 
-Presentacion()
-numerosIniciales()
-MostrarTablero()
-rellenarPosiciones()
-
-if GameOver== True:
-    print("""      
-███████╗███████╗     █████╗  ██████╗ █████╗ ██████╗  ██████╗     ███████╗██╗              ██╗██╗   ██╗███████╗ ██████╗  ██████╗ 
-██╔════╝██╔════╝    ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔═══██╗    ██╔════╝██║              ██║██║   ██║██╔════╝██╔════╝ ██╔═══██╗
-███████╗█████╗      ███████║██║     ███████║██████╔╝██║   ██║    █████╗  ██║              ██║██║   ██║█████╗  ██║  ███╗██║   ██║
-╚════██║██╔══╝      ██╔══██║██║     ██╔══██║██╔══██╗██║   ██║    ██╔══╝  ██║         ██   ██║██║   ██║██╔══╝  ██║   ██║██║   ██║
-███████║███████╗    ██║  ██║╚██████╗██║  ██║██████╔╝╚██████╔╝    ███████╗███████╗    ╚█████╔╝╚██████╔╝███████╗╚██████╔╝╚██████╔╝
-╚══════╝╚══════╝    ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═════╝  ╚═════╝     ╚══════╝╚══════╝     ╚════╝  ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝ """)
-    
-
-
-
-
-
-
-
-
+Juego=Sudoku()
+Juego.partida()
 
 
